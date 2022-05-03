148273f34c46c84e9e67b86f1ad0f8d7ac3b23f1
Mohan <vivek.mohanraj@gmail.com>
2022-05-03 14:49:33 +0800

    Add MyNFTs table in MyNFTs view

--- app_ui/src/views/MyNFTs.vue
+++ app_ui/src/views/MyNFTs.vue
@@ -43,14 +43,85 @@
 				</a-row>
 			</template>
 		</a-card>
-		</div>
 
+		<!-- My SW License NFTs Table -->
+		<a-row :gutter="24" type="flex">
+
+			<!-- My SW License NFTs Table Column -->
+			<a-col :span="24" class="mb-24">
+
+				<!-- My SW License NFTs Table Column -->
+				<MySWLicenseNFTs
+					:data="mySWLicenseNFTs.data"
+					:columns="mySWLicenseNFTs.columns"
+				></MySWLicenseNFTs>
+				<!-- / My SW License NFTs Table Column -->
+
+			</a-col>
+			<!-- / My SW License NFTs Table Column -->
+
+		</a-row>
+		<!-- / My SW License NFTs Table -->
+
+	</div>
 </template>
 
 <script>
 
 	import myNFTsController from "../controllers/MyNFTsController";
+	// "My SW License NFTs" table component.
+	import MySWLicenseNFTs from '../components/Cards/CardSWLicenseNFTs';
+
+	// "My SW License NFTs" table list of columns and their properties.
+	const mySWLicenseNFTsColumns = [
+		{
+			title: 'NFT',
+			dataIndex: 'nft',
+			scopedSlots: { customRender: 'nft' },
+		},
+		{
+			title: 'TOKEN ID',
+			dataIndex: 'tokenId',
+			class: 'font-semibold text-muted',
+		},
+		{
+			title: 'OWNER ADDRESS',
+			dataIndex: 'ownerAddress',
+			class: 'font-semibold text-muted',
+		},
+		{
+			title: 'DESCRIPTION',
+			dataIndex: 'description',
+			class: 'font-semibold text-muted',
+		},
+		{
+			title: 'CREATED AT',
+			scopedSlots: { customRender: 'created_at' },
+			dataIndex: 'created_at',
+		},
+	];
+
+	// "My SW License NFTs" table list of rows and their properties.
+	const mySWLicenseNFTsData = [
+    {
+        "key": 5,
+        "tokenId": 18,
+        "description": "Test Mint Token API Endpoint",
+		"ownerAddress": "0x0831242671918922AFeecbB4B1a37de6D21490d2",
+		"nft": {
+			"title": "Test Mint Token API Endpoint",
+			"image_url": "https://cdn.britannica.com/97/1597-004-05816F4E/Flag-India.jpg",
+		},
+        "created_at": "2022-04-25T08:38:34.179Z",
+        "updated_at": "2022-04-28T03:34:29.274Z",
+        "deletedAt": null
+    }
+];
+
 	export default ({
+		components: {
+			MySWLicenseNFTs
+		},
 		data() {
 			return {
 				walletName: "",
@@ -58,6 +129,10 @@
 				walletImageSrc: "images/face-1.jpg",
 				networkId: '',
 				account: '',
+				mySWLicenseNFTs: {
+					columns: mySWLicenseNFTsColumns,
+					data: mySWLicenseNFTsData
+				}
 			}
 		},
 		methods: {
